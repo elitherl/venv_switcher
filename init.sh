8c4985445393bbf375d1986c8706714889672944
Edward Litherland-Smith <edward.litherland-smith@ukaea.uk>
2021-04-08 09:47:36 +0100

    Add venv aliasing to init.sh

--- init.sh
+++ init.sh
@@ -4,12 +4,14 @@ if [[ -n "$BASH_VERSION" ]]; then
   if [[ ! -n "$VIRTUAL_ENV_DIR" ]]; then
     export VIRTUAL_ENV_DIR="$(dirname $(dirname $(realpath ${BASH_SOURCE[0]})))"
   fi
+  alias venv="source $VIRTUAL_ENV_DIR/scripts/venv.sh"
   source "$(dirname "${BASH_SOURCE[0]}")/completion.bash"
 
 elif [[ -n "$ZSH_VERSION" ]]; then
   if [[ ! -n "$VIRTUAL_ENV_DIR" ]]; then
     export VIRTUAL_ENV_DIR="${0:a:h:h}"
   fi
+  alias venv="source $VIRTUAL_ENV_DIR/scripts/venv.sh"
   source "${0:a:h}/completion.zsh"
 
 fi
